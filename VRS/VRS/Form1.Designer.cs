6b75c5f0e84d9088af23f05d9b88b99547de7b1c
lawrencereyes <lawrencereyes96@gmail.com>
2017-02-19 16:01:38 -0500

    Voice Recognition Software

---
+++ VRS/VRS/Form1.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace VRS
+{
+    partial class form
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtBox = new System.Windows.Forms.TextBox();
+            this.btnRead = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnRespond = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtBox
+            // 
+            this.txtBox.Location = new System.Drawing.Point(12, 12);
+            this.txtBox.Multiline = true;
+            this.txtBox.Name = "txtBox";
+            this.txtBox.Size = new System.Drawing.Size(397, 320);
+            this.txtBox.TabIndex = 0;
+            this.txtBox.TextChanged += new System.EventHandler(this.txtBox_TextChanged);
+            // 
+            // btnRead
+            // 
+            this.btnRead.Location = new System.Drawing.Point(12, 358);
+            this.btnRead.Name = "btnRead";
+            this.btnRead.Size = new System.Drawing.Size(96, 23);
+            this.btnRead.TabIndex = 1;
+            this.btnRead.Text = "Read Text";
+            this.btnRead.UseVisualStyleBackColor = true;
+            this.btnRead.Click += new System.EventHandler(this.btnRead_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(180, 358);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(75, 23);
+            this.btnClear.TabIndex = 2;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnRespond
+            // 
+            this.btnRespond.Location = new System.Drawing.Point(320, 358);
+            this.btnRespond.Name = "btnRespond";
+            this.btnRespond.Size = new System.Drawing.Size(89, 23);
+            this.btnRespond.TabIndex = 3;
+            this.btnRespond.Text = "Respond";
+            this.btnRespond.UseVisualStyleBackColor = true;
+            this.btnRespond.Click += new System.EventHandler(this.btnRespond_Click);
+            // 
+            // form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(421, 406);
+            this.Controls.Add(this.btnRespond);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnRead);
+            this.Controls.Add(this.txtBox);
+            this.Name = "form";
+            this.Text = "Voice Recognition Software";
+            this.Load += new System.EventHandler(this.form_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtBox;
+        private System.Windows.Forms.Button btnRead;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnRespond;
+    }
+}
+
---
+++ VRS/VRS/Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Speech.Recognition;
+using System.Speech.Synthesis;
+using System.Threading;
+using System.IO;
+
+namespace VRS{
+    public partial class form : Form{
+        SpeechSynthesizer sSynt = new SpeechSynthesizer();
+        SpeechRecognizer sr = new SpeechRecognizer();
+        PromptBuilder pBuilder = new PromptBuilder();
+        SpeechRecognitionEngine sRec = new SpeechRecognitionEngine();
+
+        string []greeting = new string[6] { "Hello", "hello", "How are you", "how are you", "Hi", "hi" };
+        string[] greetingRespond = new string[6] { "Hello", "Hello", "I am fine, thank you", "I am fine, thank you", "Hi" , "Hi"};
+
+        public form(){
+            InitializeComponent();
+        }
+
+        private void form_Load(object sender, EventArgs e){
+            
+        }
+
+        private void btnRead_Click(object sender, EventArgs e){
+            pBuilder.ClearContent();
+            pBuilder.AppendText(txtBox.Text);
+            sSynt.Speak(pBuilder);
+        }
+
+        private void txtBox_TextChanged(object sender, EventArgs e){
+
+        }
+
+        void sRec_SpeechRecognized(object sender, SpeechRecognizedEventArgs e){
+            
+        }
+
+        private void btnRespond_Click(object sender, EventArgs e){
+            int resp = sRecognition();
+
+            compSpeak(greetingRespond[resp]); 
+        }
+
+        int sRecognition(){
+            int i = 0;
+
+            for(; i < 3; i++) {
+                if(greeting[i] == txtBox.Text){
+                    return i;
+                }
+            }
+
+            return -1;
+        }
+
+        void compSpeak(string text){
+            sSynt.Speak(text);
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            txtBox.Text = "";
+        }
+    }
+}
