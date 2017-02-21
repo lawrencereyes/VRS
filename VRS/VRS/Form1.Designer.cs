4cbc160a19b959dcea77a47756b157b7ad353cd4
lawrencereyes <lawrencereyes96@gmail.com>
2017-02-21 06:50:10 -0500

    New version of the Voice recognition Software

--- VRS/VRS/Form1.Designer.cs
+++ VRS/VRS/Form1.Designer.cs
@@ -20,82 +20,141 @@
             base.Dispose(disposing);
         }
 
-        #region Windows Form Designer generated code
-
         /// <summary>
         /// Required method for Designer support - do not modify
         /// the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent()
         {
-            this.txtBox = new System.Windows.Forms.TextBox();
-            this.btnRead = new System.Windows.Forms.Button();
-            this.btnClear = new System.Windows.Forms.Button();
-            this.btnRespond = new System.Windows.Forms.Button();
+            this.components = new System.ComponentModel.Container();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.addWordsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.contextMenuStrip2 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.stopRecognitionToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.muteVoiceToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.contextMenuStrip2.SuspendLayout();
             this.SuspendLayout();
             // 
-            // txtBox
+            // contextMenuStrip1
             // 
-            this.txtBox.Location = new System.Drawing.Point(12, 12);
-            this.txtBox.Multiline = true;
-            this.txtBox.Name = "txtBox";
-            this.txtBox.Size = new System.Drawing.Size(397, 320);
-            this.txtBox.TabIndex = 0;
-            this.txtBox.TextChanged += new System.EventHandler(this.txtBox_TextChanged);
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.addWordsToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(153, 28);
             // 
-            // btnRead
+            // addWordsToolStripMenuItem
             // 
-            this.btnRead.Location = new System.Drawing.Point(12, 358);
-            this.btnRead.Name = "btnRead";
-            this.btnRead.Size = new System.Drawing.Size(96, 23);
-            this.btnRead.TabIndex = 1;
-            this.btnRead.Text = "Read Text";
-            this.btnRead.UseVisualStyleBackColor = true;
-            this.btnRead.Click += new System.EventHandler(this.btnRead_Click);
+            this.addWordsToolStripMenuItem.Name = "addWordsToolStripMenuItem";
+            this.addWordsToolStripMenuItem.Size = new System.Drawing.Size(152, 24);
+            this.addWordsToolStripMenuItem.Text = "Add Words";
             // 
-            // btnClear
+            // timer1
             // 
-            this.btnClear.Location = new System.Drawing.Point(180, 358);
-            this.btnClear.Name = "btnClear";
-            this.btnClear.Size = new System.Drawing.Size(75, 23);
-            this.btnClear.TabIndex = 2;
-            this.btnClear.Text = "Clear";
-            this.btnClear.UseVisualStyleBackColor = true;
-            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 5000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
-            // btnRespond
+            // pictureBox1
             // 
-            this.btnRespond.Location = new System.Drawing.Point(320, 358);
-            this.btnRespond.Name = "btnRespond";
-            this.btnRespond.Size = new System.Drawing.Size(89, 23);
-            this.btnRespond.TabIndex = 3;
-            this.btnRespond.Text = "Respond";
-            this.btnRespond.UseVisualStyleBackColor = true;
-            this.btnRespond.Click += new System.EventHandler(this.btnRespond_Click);
+            this.pictureBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1072, 566);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
+            this.pictureBox1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseUp);
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.ContextMenuStrip = this.contextMenuStrip2;
+            this.notifyIcon1.Text = "Zira";
+            this.notifyIcon1.Visible = true;
+            this.notifyIcon1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseDoubleClick);
+            // 
+            // contextMenuStrip2
+            // 
+            this.contextMenuStrip2.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.contextMenuStrip2.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.stopRecognitionToolStripMenuItem,
+            this.muteVoiceToolStripMenuItem,
+            this.toolStripSeparator1,
+            this.exitToolStripMenuItem});
+            this.contextMenuStrip2.Name = "contextMenuStrip2";
+            this.contextMenuStrip2.RenderMode = System.Windows.Forms.ToolStripRenderMode.Professional;
+            this.contextMenuStrip2.Size = new System.Drawing.Size(194, 82);
+            // 
+            // stopRecognitionToolStripMenuItem
+            // 
+            this.stopRecognitionToolStripMenuItem.Name = "stopRecognitionToolStripMenuItem";
+            this.stopRecognitionToolStripMenuItem.Size = new System.Drawing.Size(193, 24);
+            this.stopRecognitionToolStripMenuItem.Text = "Stop Recognition";
+            // 
+            // muteVoiceToolStripMenuItem
+            // 
+            this.muteVoiceToolStripMenuItem.Name = "muteVoiceToolStripMenuItem";
+            this.muteVoiceToolStripMenuItem.Size = new System.Drawing.Size(193, 24);
+            this.muteVoiceToolStripMenuItem.Text = "Mute Voice";
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(190, 6);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(193, 24);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
             // 
             // form
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(421, 406);
-            this.Controls.Add(this.btnRespond);
-            this.Controls.Add(this.btnClear);
-            this.Controls.Add(this.btnRead);
-            this.Controls.Add(this.txtBox);
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1072, 566);
+            this.ContextMenuStrip = this.contextMenuStrip1;
+            this.Controls.Add(this.pictureBox1);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "form";
-            this.Text = "Voice Recognition Software";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
             this.Load += new System.EventHandler(this.form_Load);
+            this.contextMenuStrip1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.contextMenuStrip2.ResumeLayout(false);
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
-        #endregion
+        //#endregion
 
-        private System.Windows.Forms.TextBox txtBox;
-        private System.Windows.Forms.Button btnRead;
-        private System.Windows.Forms.Button btnClear;
-        private System.Windows.Forms.Button btnRespond;
+        public System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        public System.Windows.Forms.ToolStripMenuItem addWordsToolStripMenuItem;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip2;
+        private System.Windows.Forms.ToolStripMenuItem stopRecognitionToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem muteVoiceToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
     }
 }
 
--- VRS/VRS/Form1.cs
+++ VRS/VRS/Form1.cs
@@ -11,64 +11,248 @@ using System.Speech.Recognition;
 using System.Speech.Synthesis;
 using System.Threading;
 using System.IO;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
 
 namespace VRS{
     public partial class form : Form{
+
+        //NECESSARY COMPONENTS FOR PROGRAM
+        StreamReader rCommand = new StreamReader(@"C:/ Users / Lawrence Reyes / Desktop / Lawrence / AI(Janna) / VRS / Commands.txt");
         SpeechSynthesizer sSynt = new SpeechSynthesizer();
         SpeechRecognizer sr = new SpeechRecognizer();
         PromptBuilder pBuilder = new PromptBuilder();
+        GrammarBuilder gBuilder = new GrammarBuilder();
         SpeechRecognitionEngine sRec = new SpeechRecognitionEngine();
 
-        string []greeting = new string[6] { "Hello", "hello", "How are you", "how are you", "Hi", "hi" };
-        string[] greetingRespond = new string[6] { "Hello", "Hello", "I am fine, thank you", "I am fine, thank you", "Hi" , "Hi"};
+        Point lastPosition;
+        bool exit = false;
 
+        Process pr = new Process();
+
+        //BEGIN 
         public form(){
             InitializeComponent();
+            MessageBox.Show("It works!");
         }
 
+        //PROGRAM LOADS
         private void form_Load(object sender, EventArgs e){
-            
+            startup();
         }
 
-        private void btnRead_Click(object sender, EventArgs e){
-            pBuilder.ClearContent();
-            pBuilder.AppendText(txtBox.Text);
-            sSynt.Speak(pBuilder);
+        //SETS UP THE GRAMMAR OF THE SOFTWARE
+        void startup(){
+            try{
+                gBuilder.Append(new Choices(System.IO.File.ReadAllLines(@"C:/ Users / Lawrence Reyes / Desktop / Lawrence / AI(Janna) / VRS / Commands.txt")));
+            }
+            catch{
+                MessageBox.Show("The 'commands' file is not working.");
+            }
+
+            Grammar gr = new Grammar(gBuilder);
+
+            try{
+                sRec.UnloadAllGrammars();
+                sRec.RecognizeAsyncCancel();
+                sRec.RequestRecognizerUpdate();
+                sRec.LoadGrammar(gr);
+                sRec.SpeechRecognized += sRec_SpeechRecognized;
+
+                sRec.SetInputToDefaultAudioDevice();
+                sRec.RecognizeAsync(RecognizeMode.Multiple);
+            }catch{
+                MessageBox.Show("Grammar Builder Error");
+            }
         }
 
-        private void txtBox_TextChanged(object sender, EventArgs e){
-
+        //COMMAND: LOCKS COMPUTER
+        public void lockComputer(){
+            System.Diagnostics.Process.Start(@"C:\WINDOWS\system32\rundll32.exe", "user32.dll,LockWorkStation");
         }
 
+        //ALL THE COMMANDS THAT THE COMPUTER RESPONDS TO
         void sRec_SpeechRecognized(object sender, SpeechRecognizedEventArgs e){
-            
-        }
-
-        private void btnRespond_Click(object sender, EventArgs e){
-            int resp = sRecognition();
-
-            compSpeak(greetingRespond[resp]); 
-        }
-
-        int sRecognition(){
-            int i = 0;
-
-            for(; i < 3; i++) {
-                if(greeting[i] == txtBox.Text){
-                    return i;
+            if(exit){
+                Thread.Sleep(100);
+                if(e.Result.Text == "yes"){
+                    sSynt.SpeakAsyncCancelAll();
+                    sRec.RecognizeAsyncCancel();
+                    Application.Exit();
+                }else{
+                    exit = false;
+                    compSpeak("Exit Cancelled");
                 }
             }
 
-            return -1;
+            switch (e.Result.Text)
+            {
+                case "hello":
+                    compSpeak("Hello Lawrence");
+                    break;
+
+                case "hi":
+                    compSpeak("Hello Lawrence");
+                    break;
+
+                case "exit":
+                    compSpeak("Are you sure you want to exit?");
+                    exit = true;
+                    break;
+
+                case "thank you":
+                    compSpeak("Anything for you Lawrence");
+                    break;
+
+                case "thanks":
+                    compSpeak("Anything for you Lawrence");
+                    break;
+
+                case "lock computer":
+                    lockComputer();
+                    break;
+
+                case "stop listening":
+                    compSpeak("OK");
+                    sRec.RecognizeAsyncCancel();
+                    sRec.RecognizeAsyncStop();
+                    break;
+
+                case "open google":
+                    compSpeak("Right away");
+                    pr.StartInfo.FileName = "https://google.ca/";
+                    pr.Start();
+                    break;
+
+                case "maximize":
+                    try{
+                        ActiveForm.WindowState = FormWindowState.Maximized;
+                        break;
+                    }catch{
+                        compSpeak("Im already mazimized Lawrence");
+                        break;
+                    }
+            }
         }
 
+        //IT ALLOWS THE SOFTWARE TO REPLY TO THE USER
         void compSpeak(string text){
-            sSynt.Speak(text);
+            sRec.RecognizeAsyncCancel();
+            sRec.RecognizeAsyncStop();
+            pBuilder.ClearContent();
+            pBuilder.AppendText(text.ToString());
+            sSynt.SelectVoiceByHints(VoiceGender.Female, VoiceAge.Adult);
+            sSynt.SpeakAsync(pBuilder);
+            sRec.RecognizeAsyncCancel();
+            sRec.RecognizeAsyncStop();
+            sRec.RecognizeAsync(RecognizeMode.Multiple);
         }
 
-        private void btnClear_Click(object sender, EventArgs e)
+        /*EXTRA*/
+        private void button3_Click(object sender, EventArgs e)
         {
-            txtBox.Text = "";
+            sRec.RecognizeAsyncCancel();
+            sRec.RequestRecognizerUpdate();
+            sRec.UnloadAllGrammars();
+
+
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            pBuilder.ClearContent();
+            sRec.RecognizeAsyncCancel();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == System.Windows.Forms.MouseButtons.Left)
+            {
+                lastPosition = e.Location;
+            }
+            else { return; }
+        }
+
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Button == System.Windows.Forms.MouseButtons.Left)
+            {
+                this.Left += e.X - lastPosition.X;
+                this.Top += e.Y - lastPosition.Y;
+            }
+        }
+
+        private void pictureBox1_MouseUp(object sender, MouseEventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            sSynt.SpeakAsyncCancelAll();
+        }
+
+
+
+
+
+
+
+        internal void cancelSpeech()
+        {
+            sSynt.SpeakAsyncCancelAll();
+        }
+
+
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            sRec.RecognizeAsync(RecognizeMode.Multiple);
+            //     btnStop.Visible = false; 
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+
+            //    name = comboBox1.Text.ToString();
+            //    sSynth.SelectVoice(comboBox1.Text);
+
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void button1_Click_2(object sender, EventArgs e)
+        {
+        
+        }
+
+        private void notifyIcon1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+
+
+
         }
     }
 }
